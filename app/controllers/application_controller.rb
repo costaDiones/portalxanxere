b9706723740501d1073930620aace91e775eb736
fboscato <fboscato@gmail.com>
2016-02-26 16:26:00 -0300

    Fim do divise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   before_filter :update_sanitized_params, if: :devise_controller?
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:nome,:email,:password,:password_confirmation)}
-    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:nome)}   
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:nome,:email,:password,:password_confirmation)}   
   end
 end
